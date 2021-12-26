7bf9133c8500429c7f74003d5d4b5ab684f9b09a
therealjustindude <therealjustindude@gmail.com>
2021-12-26 17:58:47 -0500

    model update to include CliqueType and isHero

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -8,14 +8,63 @@ import (
 	"example/outerbanks-api/graph/generated"
 	"example/outerbanks-api/graph/model"
 	"fmt"
+	"strconv"
 )
 
 func (r *mutationResolver) UpsertCharacter(ctx context.Context, input model.CharacterInput) (*model.Character, error) {
-	panic(fmt.Errorf("not implemented"))
+	id := input.ID
+	var character model.Character
+	character.Name = input.Name
+	character.CliqueType = input.CliqueType
+
+	n := len(r.Resolver.CharacterStore)
+	if n == 0 {
+		r.Resolver.CharacterStore = make(map[string]model.Character)
+	}
+
+	if id != nil {
+		cs, ok := r.Resolver.CharacterStore[*id]
+		if !ok {
+			return nil, fmt.Errorf("not found")
+		}
+		if input.IsHero != nil {
+			character.IsHero = *input.IsHero
+		} else {
+			character.IsHero = cs.IsHero
+		}
+		r.Resolver.CharacterStore[*id] = character
+	} else {
+		// generate unique id
+		nid := strconv.Itoa(n + 1)
+		character.ID = nid
+		if input.IsHero != nil {
+			character.IsHero = *input.IsHero
+		}
+		r.Resolver.CharacterStore[nid] = character
+	}
+
+	return &character, nil
 }
 
 func (r *queryResolver) Character(ctx context.Context, id string) (*model.Character, error) {
-	panic(fmt.Errorf("not implemented"))
+	character, ok := r.Resolver.CharacterStore[id]
+	if !ok {
+		return nil, fmt.Errorf("not found")
+	}
+	return &character, nil
+}
+
+func (r *queryResolver) Characters(ctx context.Context, cliqueType model.CliqueType) ([]*model.Character, error) {
+	characters := make([]*model.Character, 0)
+	for idx := range r.Resolver.CharacterStore {
+		character := r.Resolver.CharacterStore[idx]
+		if character.CliqueType == cliqueType {
+
+			characters = append(characters, &character)
+		}
+	}
+
+	return characters, nil
 }
 
 func (r *queryResolver) Pogues(ctx context.Context) ([]*model.Character, error) {
